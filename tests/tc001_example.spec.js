363b1083353f0d4f12df9c2d037186960dbda748
Sivaram <sivaram579@gmail.com>
2025-04-17 22:17:23 +0530

    implemented POM

--- tests/tc001_example.spec.js
+++ tests/tc001_example.spec.js
@@ -6,6 +6,7 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+  await page.close();
 });
 
 test('get started link', async ({ page }) => {
@@ -16,4 +17,5 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.close();
 });
