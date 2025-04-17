9faa2af128f293826c3ad529c429ec87f10e7737
Sivaram <sivaram579@gmail.com>
2025-04-17 20:19:57 +0530

    first commit

---
+++ tests/loginandplaceorderinsacedemo.spec.js
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test('Login and place order in SauceDemo app', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await expect(page.locator('#root')).toContainText('Swag Labs');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="item-4-title-link"] [data-test="inventory-item-name"]')).toContainText('Sauce Labs Backpack');
+  await page.locator('[data-test="item-4-title-link"]').click();
+  await expect(page.locator('[data-test="inventory-item-name"]')).toContainText('Sauce Labs Backpack');
+  await page.locator('[data-test="add-to-cart"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await expect(page.locator('[data-test="item-4-title-link"]')).toBeVisible();
+  await page.locator('[data-test="checkout"]').click();
+  await expect(page.locator('[data-test="title"]')).toBeVisible();
+  await page.locator('[data-test="firstName"]').click();
+  await page.locator('[data-test="firstName"]').fill('Siva');
+  await page.locator('[data-test="lastName"]').click();
+  await page.locator('[data-test="lastName"]').fill('Ram');
+  await page.locator('[data-test="postalCode"]').click();
+  await page.locator('[data-test="postalCode"]').fill('500049');
+  await page.locator('[data-test="continue"]').click();
+  await expect(page.locator('[data-test="total-label"]')).toBeVisible();
+  await page.locator('[data-test="finish"]').click();
+  await expect(page.locator('[data-test="complete-header"]')).toContainText('Thank you for your order!');
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+  await expect(page.getByText('Swag Labs')).toBeVisible();
+  await page.close();
+});
