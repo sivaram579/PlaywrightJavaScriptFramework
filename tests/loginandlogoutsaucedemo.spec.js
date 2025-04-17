e238c8369eae06a2c9e1a13d0df5a706aadf6de4
Sivaram <sivaram579@gmail.com>
2025-04-17 20:27:00 +0530

    New test case - Login and Logout Sauce Demo

---
+++ tests/loginandlogoutsaucedemo.spec.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('Login and place order in SauceDemo app', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await expect(page.locator('#root')).toContainText('Swag Labs');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="item-4-title-link"] [data-test="inventory-item-name"]')).toContainText('Sauce Labs Backpack');
+  
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+  await expect(page.getByText('Swag Labs')).toBeVisible();
+  await page.close();
+});
